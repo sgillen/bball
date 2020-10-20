1af1f5a48a1d4b9e92de731b9b39d0d2c94829f2
sgillen <sgillen@ucsb.edu>
2020-10-19 17:45:46 -0700

    new dim file

--- matlab/dimensionality.m
+++ matlab/dimensionality.m
@@ -1,7 +1,12 @@
 % load itall x_impacts
 % x4 = x_impacts';
 %load bball_dim.mat
-x4 = state_mat(2:end,2:end);
+% x4 = state_mat(2:end,2:end);
+x4 = state_mat;
+x_axis = 1:1:size(x4,1);
+plot(x_axis, x4(:, 10))
+figure
+plot(x_axis, x4(:, 5))
 
 %load itall_2 x_impacts
 %x4 = [x4; x_impacts'];
@@ -11,18 +16,15 @@ x4 = state_mat(2:end,2:end);
 xmin = min(x4);
 xmax = max(x4);
 sf = 1./(xmax-xmin);
-% % for n=1:3
-% %     x4(:,n) = sf(n)*x4(:,n);
-% % end
-% 
+% for n=1:3
+%     x4(:,n) = sf(n)*x4(:,n);
+% end
 
 for n=1:size(x4,2)
     x4(:,n) = sf(n)*x4(:,n);
 end
 
-%x4 = (x4 - mean(x4))./std(x4)
-
-dlist = 10.^[2:-.1:-4];
+dlist = 10.^[2:-.25:-4];
 nlist = 0*dlist; % to be determined, below
 for ndo = 1:length(dlist)   
     Mid = zeros(1,length(x4(:,1)));
@@ -51,7 +53,7 @@ for ndo = 1:length(dlist)
     nlist(ndo) = mi
 end
 
-use_id = [1:15]+15; % only fit a line to first n points, in dlist....
+use_id = [1:6]+7; % only fit a line to first n points, in dlist....
 pp = polyfit(log(dlist(use_id)),log(nlist(use_id)),1)
 frac_dim = -pp(1)
 
@@ -65,4 +67,4 @@ loglog(xfit,0*yfit+1,'b--')
 loglog(dlist(use_id),nlist(use_id),'ro','MarkerFaceColor','k'); 
 
 xlabel('d_{thr}, threshold radius')
-ylabel('N, number of mesh points') 
+ylabel('N, number of mesh points')
