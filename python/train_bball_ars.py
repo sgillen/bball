8a3d841128e83aaaf3fab1231d04d8f39fc0831f
Aditya <asattawar@gmail.com>
2021-07-04 12:57:04 -0700

    fix concurrency issue with freeze_support, replace torch handling with pickle handling

--- python/train_bball_ars.py
+++ python/train_bball_ars.py
@@ -2,12 +2,16 @@ import gym
 import numpy as np
 from numpy import pi
 from seagul.rl.ars import ARSAgent
-from seagul.nn import MLP
-import torch
+from multiprocessing import Process,Queue, freeze_support
+# from seagul.nn import MLP
+# import torch
+import time
 import pickle
 import os
 
 from bball3_env import BBall3Env
+
+
 # init policy, valuefn
 input_size = 10
 output_size = 3
@@ -16,7 +20,7 @@ env_name = "bball3-v1"
 num_steps = int(1e5)
 base_dir = os.path.dirname(__file__) + "./data_ars2/"
 trial_name = input("Trial name: ")
-torch.set_default_dtype(torch.float32)
+# torch.set_default_dtype(torch.float32)
 
 
 
@@ -48,20 +52,27 @@ os.makedirs(trial_dir, exist_ok=False)
 with open(trial_dir + "config.pkl", "wb") as config_file:
     pickle.dump(env_config, config_file)
 
+start_time = time.time()
 
-for seed in np.random.randint(0, 2**32, 4):
-    alg_config = {
-        "env_name": env_name,
-        "seed": int(seed),  # int((time.time() % 1)*1e8),
-        "env_config" : env_config,
-        "n_workers" : 2,
-        "exp_noise" : .025
-    }
-    agent = ARSAgent(**alg_config)
-    #    num_epochs = 1
-    total_steps = 500
+if __name__ == "__main__":
+    freeze_support()
+    for seed in np.random.randint(0, 2**32, 1):
+        alg_config = {
+            "env_name": env_name,
+            "seed": int(seed),  # int((time.time() % 1)*1e8),
+            "env_config" : env_config,
+            "n_workers" : 1,
+            "exp_noise" : .025
+        }
+        # ARSAGent --> USER DEFINED class defining all the RL stuff
+        agent = ARSAgent(**alg_config)
+        #    num_epochs = 1
+        total_steps = 1500
 
-    #   for epoch in range(num_epochs):
-    agent.learn(int(total_steps))
-    torch.save(agent, f"{trial_dir}{seed}.agent")
+        #   for epoch in range(num_epochs):
+        agent.learn(int(total_steps))
+        saveFile = open(f"{trial_dir}{seed}.agent", 'wb')
+        pickle.dump(agent, saveFile)
 
+end_time = time.time()
+print("Time taken to train (in seconds): ", (end_time - start_time))
